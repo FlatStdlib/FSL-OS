4152e0c4645571bfc666ee70c62ce1d8f21dc04c
Algorithms <142917637+Algo1337@users.noreply.github.com>
2026-03-10 10:39:21 -0500

    rm junk

--- src/tools/variables.c
+++ src/tools/variables.c
@@ -1,10 +1,4 @@
 #include "../fsl_efi.h"
 
 public bool set_new_variable(const string name, const string value)
-{
-    if(!name || !value)
-        return false;
-
-    
-    return map_append(_FSLEFI_->variables, name, value);
-}
+{ return map_append(_FSLEFI_->variables, name, value); }
